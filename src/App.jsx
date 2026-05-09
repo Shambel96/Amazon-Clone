754bdd2c7817ab0490c6025321cbd2e6fd6158b4
Shambel Dechu <shambeldechu@gmail.com>
2026-05-09 15:04:25 +0300

    chore: import and install carousel dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import './App.css'
 import Header from './Components/Header/Header.jsx'
+import Carousel from "./Components/Carousel/Carousel.jsx";
 
 function App() {
 
   return (
   <div>
     <Header />
+    <Carousel />
   </div>
   )
 }
