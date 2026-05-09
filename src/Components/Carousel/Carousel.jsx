5aa7900dd4d1d314ecb5945f3b63f86b0384ed7e
Shambel Dechu <shambeldechu@gmail.com>
2026-05-09 15:03:20 +0300

    feat: add crousel effect

---
+++ src/Components/Carousel/Carousel.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import { img } from "./Img/data";
+import classes from "./Carousel.module.css";
+
+function CarouselEffect() {
+  return (
+    <div className={classes.carousel}>
+      <Carousel
+        autoplay={true}
+        infiniteLoop={true}
+        showThumbs={false}
+        showIndicators={false}
+        interval={200}
+      >
+        {img.map((imgLink, index) => (
+          <img key={index} src={imgLink} alt={`Slide ${index + 1} image`} />
+        ))}
+      </Carousel>
+      <div className={classes.hero_img}></div>
+    </div>
+  );
+}
+
+export default CarouselEffect;
