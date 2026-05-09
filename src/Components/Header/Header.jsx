f8dc1d361f904b0c4c2b817f3aaed7d5aab62788
Shambel Dechu <shambeldechu@gmail.com>
2026-05-09 13:02:11 +0300

    feat: add header

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import classes from "./Header.module.css";
+import { FaSearch } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
+import { SlLocationPin } from "react-icons/sl";
+import LowerHeader from "./LowerHeader";
+const Header = () => {
+  return (
+    <>
+      <section>
+        <div className={classes.header_container}>
+          <div className={classes.logo_container}>
+            <a href="#">
+              <img
+                src="https://pngimg.com/uploads/amazon/small/amazon_PNG11.png"
+                alt="amazon logo"
+              />
+            </a>
+            <div className={classes.delivery_info}>
+              <span>
+                <SlLocationPin />
+              </span>
+              <div>
+                <p>Deliver to</p>
+                <span>Ethiopia</span>
+              </div>
+            </div>
+          </div>
+          <div className={classes.search_container}>
+            <select name="" id="">
+              <option value="">All</option>
+            </select>
+            <input type="text" placeholder="Search Product" />
+            <FaSearch size={38} />
+          </div>
+          <div className={classes.order_container}>
+            <a href="" className={classes.language}>
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Flag_of_the_United_States_%28DDD-F-416E_specifications%29.svg/1920px-Flag_of_the_United_States_%28DDD-F-416E_specifications%29.svg.png"
+                alt=""
+              />
+              <select name="" id="">
+                <option value="">EN</option>
+              </select>
+            </a>
+
+            <a href="">
+              <p>Sign In</p>
+              <span>Account and Lists</span>
+            </a>
+            <a href="">
+              <p>Returns</p>
+              <span>& Orders</span>
+            </a>
+            <a href="" className={classes.cart}>
+              <FaShoppingCart size={35} />
+              <span>0</span>
+            </a>
+          </div>
+        </div>
+      </section>
+      <LowerHeader />
+    </>
+  );
+};
+
+export default Header;
---
+++ src/Components/Header/LowerHeader.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { IoMenu } from "react-icons/io5";
+import classes from "./Header.module.css";
+const LowerHeader = () => {
+  return (
+    <div className={classes.lower_header}>
+      <ul>
+        <li>
+            <IoMenu />
+          <p>All</p>
+        </li>
+        <li>Today's deals</li>
+        <li>Costumer Services</li>
+        <li>Registry</li>
+        <li>Gift cards</li>
+        <li>Sell</li>
+      </ul>
+    </div>
+  );
+};
+
+export default LowerHeader;
