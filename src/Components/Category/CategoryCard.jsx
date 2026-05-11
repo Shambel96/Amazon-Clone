71a6d19f100bf14403cf4f7a00988044a7482a60
Shambel Dechu <shambeldechu@gmail.com>
2026-05-12 01:11:29 +0300

    feat: add responsive to differebt screen size  ad animation when hovered

--- src/Components/Category/CategoryCard.jsx
+++ src/Components/Category/CategoryCard.jsx
@@ -6,13 +6,15 @@ import { Link } from "react-router-dom";
 export default function CategoryCard({ category }) {
   return (
     <div className={classes.category}>
-      <Link to={`/categories/${category.name}`}>
+      <hr />
+      <Link className={classes.categoryLink} to={`/categories/${category.name}`}>
         <span>
           <h2>{category.title}</h2>
         </span>
         <img src={category.imageLink} alt="category image" />
         <p>Shop now</p>
       </Link>
+      <hr />
     </div>
   );
 }
