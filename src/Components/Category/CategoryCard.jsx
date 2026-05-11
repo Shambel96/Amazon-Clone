e92e413a0100301911fae2891498c0f77b3b6a66
Shambel Dechu <shambeldechu@gmail.com>
2026-05-12 00:43:52 +0300

    feat: refactor: replace anchor tag with link

--- src/Components/Category/CategoryCard.jsx
+++ src/Components/Category/CategoryCard.jsx
@@ -1,20 +1,18 @@
 import React from "react";
 import Category from "./Category";
 import classes from "./category.module.css";
+import { Link } from "react-router-dom";
 
 export default function CategoryCard({ category }) {
   return (
     <div className={classes.category}>
-      <a href="#">
+      <Link to={`/categories/${category.name}`}>
         <span>
           <h2>{category.title}</h2>
         </span>
-        <img
-          src={category.imageLink}
-          alt="category image"
-        />
+        <img src={category.imageLink} alt="category image" />
         <p>Shop now</p>
-      </a>
+      </Link>
     </div>
   );
 }
--- src/Components/Category/CategoryInfos.js
+++ src/Components/Category/CategoryInfos.js
@@ -1,11 +1,11 @@
 export const categoryInfo = [
     {
         title: 'Electronics',
-        name: 'Electronics',
+        name: 'electronics',
         imageLink: 'https://fakestoreapi.com/img/81QpkIctqPL._AC_SX679_t.png'
     },  
     {
-        title: "men's clothing",
+        title: "Men's Clothing",
         name: "men's clothing",
         imageLink: "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_t.png"
     },
@@ -15,7 +15,7 @@ export const categoryInfo = [
         imageLink: 'https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_t.png'
     },
     {
-        title: 'jewelery',
+        title: 'Jewelery',
         name: 'jewelery',
         imageLink: 'https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_t.png'
     }]
