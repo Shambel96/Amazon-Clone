ee73662ffb21a8050e7348eb7cf18d752ab0e095
Shambel Dechu <shambeldechu@gmail.com>
2026-05-12 13:33:34 +0300

    feat:add loader to product

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -1,23 +1,37 @@
-import React, {useEffect, useState} from 'react'
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import classes from "./Product.module.css";
 import ProductCard from "./ProductCard";
+import Loader from "../Loader/Loader";
+import LayOut from "../../Layout/LayOut";
 function Product() {
   const [products, setProducts] = useState([]);
-
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
-    axios.get('https://fakestoreapi.com/products')
-      .then(response => setProducts(response.data))
-      .catch(error => console.error('Error fetching products:', error));
-  }, []);
-return (
-    <div className={classes.product_container}>
-      {products.map(product => (
-        <ProductCard key={product.id} product={product} />
-      ))}
-    </div>
+    setLoading(true);
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((response) => setProducts(response.data),
+    setLoading(false)
   )
+      .catch((error) => console.error("Error fetching products:", error),
+    setLoading(false));
+  }, []);
+  return (
+    <>
+      {loading ? (
+        <div style={{ display: "flex", justifyContent: "center", marginTop: "50px" }}>
+          <Loader />
+        </div>
+      ) : (
+        <div className={classes.product_container}>
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      )}
+    </>
+  );
 }
 
-export default Product
-          
+export default Product;
