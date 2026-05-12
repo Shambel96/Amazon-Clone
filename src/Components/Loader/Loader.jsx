22c585c502705b1ab7988eccbc9d9032facec541
Shambel Dechu <shambeldechu@gmail.com>
2026-05-12 13:32:55 +0300

    feat: add loader

---
+++ src/Components/Loader/Loader.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {ClipLoader} from "react-spinners";
+function Loader() {
+  return (
+    <div style={{
+      display:"center",
+      alignItems: "center",
+      justifyContent:"center",
+      height: "50vh"
+    }}>
+      <ClipLoader />
+    </div>
+  )
+}
+
+export default Loader
