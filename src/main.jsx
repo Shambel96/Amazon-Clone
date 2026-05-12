6ea7b12299c133e1b618becabb00811d2109296b
Shambel Dechu <shambeldechu@gmail.com>
2026-05-13 00:57:09 +0300

    feat: import data provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {DataProvider} from "./Components/DataProvider/DataProvider";
+import { initialState, reducer } from "./Utility/reducer";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <DataProvider initialState={initialState} reducer={reducer}>
     <App />
+    </DataProvider>
   </StrictMode>,
 )
+
