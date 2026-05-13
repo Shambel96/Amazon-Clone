0e328da3a4e241920bdece4ca6288545bc60b937
Shambel Dechu <shambeldechu@gmail.com>
2026-05-13 15:29:32 +0300

    refactor: collect the landing page components into single landing folder component

---
+++ src/Pages/Landing/Landing.jsx
@@ -0,0 +1,16 @@
+import CrouselEffect from '../../Components/Carousel/Carousel';
+import Category from '../../Components/Category/Category';
+import Product from '../../Components/Product/Product';
+import LayOut from '../../Layout/LayOut';
+
+function Landing() {
+  return (
+    <LayOut>
+      <CrouselEffect />
+      <Category />
+      <Product />
+    </LayOut>
+  )
+}
+
+export default Landing
