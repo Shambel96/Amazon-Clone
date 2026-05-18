964d6849fb4a3f16f59b6c5b4cb6d0cbd6f1ba27
Shambel Dechu <shambeldechu@gmail.com>
2026-05-18 16:46:05 +0300

    feat: add authentication with sign in and up within UI

---
+++ src/Pages/Auth/Auth.jsx
@@ -0,0 +1,124 @@
+import React, { useState, useContext } from "react";
+import classes from "./Auth.module.css";
+import amazonLogo from "../../assets/amazon-logo.jpg";
+import { Link, useNavigate } from "react-router-dom";
+import { auth } from "../../Utility/firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { DataContext } from "../../Components/DataProvider/DataProvider";
+import { type } from "../../Utility/action.type";
+import { ClipLoader } from "react-spinners";
+function Auth() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState({
+    signin: false,
+    signup: false,
+  });
+  const navigate = useNavigate();
+  //console.log(email, password);
+  const { state, dispatch } = useContext(DataContext);
+  //console.log(state.user);
+  const { user } = state;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!email || !password) {
+      setError("Please fill in all fields");
+      //console.log('Please fill in all fields');
+      return;
+    }
+    if (e.target.name === "signin") {
+      setLoading({ ...loading, signin: true });
+      signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          dispatch({ type: "SET_USER", user: userCredential.user });
+          setLoading({ ...loading, signin: false });
+          navigate("/");
+        })
+        .catch((error) => {
+          setError(error.message);
+          setLoading({ ...loading, signin: false });
+        });
+    } else {
+      setLoading({ ...loading, signup: true });
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          dispatch({ type: "SET_USER", user: userCredential.user });
+          setLoading({ ...loading, signup: false });
+          navigate("/auth");
+        })
+        .catch((error) => {
+          setError(error.message);
+          setLoading({ ...loading, signup: false });
+        });
+    }
+  };
+  return (
+    <>
+      <div className={classes.auth_Container}>
+        <Link to="/">
+          <img src={amazonLogo} alt="amazon-logo" />
+        </Link>
+        <div className={classes.auth_Form}>
+          <h2>Sign In {error && <p style={{ color: "red" }}>{error}</p>}</h2>
+          <form action="">
+            <div>
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <button type="submit" name="signin" onClick={handleSubmit}>
+              {loading.signin ? <ClipLoader size={15} /> : "Sign In"}
+            </button>
+          </form>
+          {/*Agreement*/}
+          <p>
+            By signing in, you agree to our Terms and Privacy Policy of Amazon
+            fake clone with cookies and local storage.
+          </p>
+          <button
+            className={classes.signUp}
+            name="signup"
+            onClick={handleSubmit}
+          >
+            {loading.signup ? (
+              <ClipLoader size={15} />
+            ) : (
+              "Create Your Amazon Account"
+            )}
+          </button>
+        </div>
+      </div>
+      <hr />
+      <div className={classes.underlineFooter}>
+        <div className={classes.footer_links}>
+          <p>Conditions of use</p>
+          <p>Privacy notice</p>
+          <p>Help</p>
+        </div>
+
+        <div>
+          <p>© 1996-2024, Amazon-clone.com, Inc. or its affiliates</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Auth;
