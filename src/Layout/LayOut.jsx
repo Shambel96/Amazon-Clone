3481a856f7324267ada9e77b5a63d8afafaa65a7
Shambel Dechu <shambeldechu@gmail.com>
2026-05-29 13:49:24 +0300

    feat: add footer into layout to amke it constant in all pages

--- src/Layout/LayOut.jsx
+++ src/Layout/LayOut.jsx
@@ -1,9 +1,12 @@
 import Header from '../Components/Header/Header';
+import Footer from '../Components/Footer/Footer';
+
 function LayOut({ children }) {
   return (
     <div>
       <Header />
       {children}
+      <Footer />
     </div>
   )
 }
