84238f17f014d93a9c64a4a9f0003e1859798108
Shambel Dechu <shambeldechu@gmail.com>
2026-06-01 17:18:40 +0300

    docs: make responsive

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
 import classes from "./Product.module.css";
 import ProductCard from "./ProductCard";
@@ -39,6 +40,30 @@ function Product() {
           ))}
         </div>
       )}
+      <div style={{ backgroundColor: "#fff", padding: "20px 0", textAlign: "center" }}>
+        <hr />
+        <h1 style={{padding:"30px 0 15px 0"}}>See personalized recommendations</h1>
+        <Link to="/auth" style={{ textDecoration: "none" }}>
+          <button
+            style={{
+              width: "250px",
+              padding: "7px",
+              backgroundColor: "#FFCE12",
+              border: "1px solid #d49644",
+              borderRadius: "20px",
+              cursor: "pointer",
+              fontSize: "16px",
+              color: "black",
+            }}
+          >
+            Sign in
+          </button>
+        </Link>
+        <p style={{padding:"5px 0 15px 0"}}>
+          New Customer? <Link to="/auth" style={{ color: "blue" }}>Start here</Link>
+        </p>
+        <hr />
+      </div>
     </>
   );
 }
